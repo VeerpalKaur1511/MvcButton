87513a35609bbfdcca7d976d9a91d866c5df514d
W0826070 <W0826070@ADL-A1201I-07>
2023-10-13 11:15:28 -0400

    updated assignment

--- Models/Button.cs
+++ Models/Button.cs
@@ -8,13 +8,32 @@ namespace MvcButton.Models
     {
         
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)] //Add validation rules like Required, StringLength
+        [Required]
         public string Title { get; set; }
+
+        [StringLength(40, MinimumLength = 1)]
+        [Required]
         public decimal Size { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Materials { get; set; }
+
+        [StringLength(70, MinimumLength = 4)]
+        [Required]
         public string Color { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
